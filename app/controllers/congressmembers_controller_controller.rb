dc8361ce8f9966c106c65d0c1f80910a03f85979
ARILANDAU <ariellelandau360@gmail.com>
2017-03-19 12:50:58 -0400

    ititial commit

---
+++ app/controllers/congressmembers_controller_controller.rb
@@ -0,0 +1,2 @@
+class CongressmembersController < ApplicationController
+end
---
+++ spec/controllers/congressmembers_controller_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe CongressmembersControllerController, type: :controller do
+
+end
