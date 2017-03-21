c90836a213e37b989ca7c4b163535fba9518f2b6
ARILANDAU <ariellelandau360@gmail.com>
2017-03-20 20:41:55 -0400

    completes devise

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,41 +1,40 @@
 require 'rails_helper'
-# [] I must specify a valid email address
-# [] I must specify a password and confirm that password
-# [] If I do not perform the above I get an error messages
-# [] If I input valid information, I register my account and get authenticated
+# [] I must provide valid information
+# [] I should see an error message if I enter an invalid email
+# [] I should see an error message if I enter an invalid password
 
-feature 'sign up' do
-  scenario 'specifying a valid and required information' do
-    visit root_path
-    click_link 'Sign Up'
-    fill_in 'First Name', with: 'Lisa'
-    fill_in 'Last Name', with: 'Simpson'
-    fill_in 'Email', with: 'lisasimpson2020@gmail.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password Confirmation', with: 'password'
-    click_button 'Sign Up'
-    expect(page).to have_content("You're in!")
+feature 'user signs in' do
+  scenario 'user successfully signs in' do
+    user = FactoryGirl.create(:user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Submit'
+
+    expect(page).to have_content('Signed in successfully.')
+    expect(page).to_not have_content('Sign In')
+    expect(page).to_not have_content('Sign Up')
     expect(page).to have_content('Sign Out')
   end
 
-  scenario 'required information is not supplied' do
-    visit root_path
-    click_link 'Sign Up'
-    click_button 'Sign Up'
+  scenario 'user provides incorrect email' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'wrongemail@email.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Submit'
 
-    expect(page).to have_content("can't be blank")
-    expect(page).to_not have_content('Sign Out')
+    expect(page).to_not have_content('Signed in successfully.')
+    expect(page).to have_content('Sign In')
   end
 
-  scenario 'password confirmation doesn\'t match password' do
-    visit root_path
-    click_link 'Sign Up'
+  scenario 'user provides incorrect password' do
+    user = FactoryGirl.create(:user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'notmypassword'
+    click_button 'Submit'
 
-    fill_in 'Password', with: 'password'
-    fill_in 'Password Confirmation', with: 'bad_password'
-    click_button 'Sign Up'
-
-    expect(page).to have_content("doesn't match")
-    expect(page).to_not have_content('Sign Out')
+    expect(page).to have_content("Sign In")
+    expect(page).to_not have_content('Signed in successfully.')
   end
 end
