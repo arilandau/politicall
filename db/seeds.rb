e4aeb35d8cf8d456e0a06507cc55aeb4c8ed3fe0
ARILANDAU <ariellelandau360@gmail.com>
2017-03-22 19:35:26 -0400

    adds test photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,8 @@ Congressmember.create!(
     url: "yahoo.com",
     next_election: "2017",
     phone_number: "123-456-7891",
-    state: "New Jersey")
+    state: "New Jersey",
+    photo: "http://i.lv3.hbo.com/assets/images/series/the-sopranos/character/tony-soprano-1920.jpg")
 
   Congressmember.create!(
     first_name: "Miss",
