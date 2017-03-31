aa603f4be961c3b3c7a779e5643ddfd27ab8e042
ARILANDAU <ariellelandau360@gmail.com>
2017-03-31 11:11:51 -0400

    fixes phone validations

--- app/models/congressmember.rb
+++ app/models/congressmember.rb
@@ -6,7 +6,6 @@ class Congressmember < ApplicationRecord
   validates :email, uniqueness: true, allow_nil: true, format: { with: /\A\w+\@[a-z]+\.[a-z]{3}\z/ }
   validates :next_election, presence: true, length: { is: 4 }, numericality: { greater_than: 2016 }
   validates :state, presence: true
-  validates :phone, uniqueness: true, allow_nil: true, format: { with: /\A[0-9]{3}\-[0-9]{3}\-[0-9]{3}\z/ }
 
   has_many :congresslists
   has_many :lists, through: :congresslists
